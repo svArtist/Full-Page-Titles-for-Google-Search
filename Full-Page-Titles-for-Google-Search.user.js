20a6e2eec4d75e03a360213611a799397eb3f2ff
svArtist <benjamin_philipp@gmx.de>
2017-02-16 01:39:29 +0100

    Update Full-Page-Titles-for-Google-Search.user.js

--- Full-Page-Titles-for-Google-Search.user.js
+++ Full-Page-Titles-for-Google-Search.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name		Retrieve Full Page Titles in Google Search
-// @version		1.5
+// @version		1.4
 // @downloadURL	https://github.com/svArtist/Full-Page-Titles-for-Google-Search/raw/master/Full-Page-Titles-for-Google-Search.user.js
 // @namespace	Google
 // @author		Benjamin Philipp <benjamin_philipp [at - please don't spam] gmx.de>
@@ -17,11 +17,11 @@
 // SETTINGS:
 var settings = {};
 settings.applyToLinkText = false;			// Default = FALSE. TRUE = change innerHTML of links and applying overflow: visible to parent; false = only apply to Link Title (for mouseover Tooltip)
-settings.rex = "<title([^>]*)>([^<]+)<";			// Default = /<title>([^<]+)</i. Regex to find the title of a page. If you find a better way, please let me know.
+settings.rex = "<title([^>]*)>([^<]+)<";	// Default = "<title([^>]*)>([^<]+)<". Regex to find the title of a page. If you find a better way, please let me know.
 settings.dontLookupExtensions = [".pdf"];	// Default = [".pdf"]. Exclude from lookup. PDFs are generally downloaded as files, giving you a popup. Excluding ".pdf" is recommended.
-settings.verbose = 2;						// Default = 1. 0 = no logs; 1 = reports on link counts; 2 = +statuses of link checks; 3 = +Details
+settings.verbose = 1;						// Default = 1. 0 = no logs; 1 = reports on link counts; 2 = +statuses of link checks; 3 = +Details
 settings.keepSettings = true;				// Default = TRUE. TRUE = Try to save & load settings in browser's localStorage. FALSE = settings will be overweitten on update.
-settings.warnOnChange = false;				// Default = TRUE. TRUE = When changes are made but the Version number stays the same (assume changes by user), ask to save and apply the settings. FALSE = Automatically apply changes.
+settings.warnOnChange = true;				// Default = TRUE. TRUE = When changes are made but the Version number stays the same (assume changes by user), ask to save and apply the settings. FALSE = Automatically apply changes.
 
 // Script vars, best don't touch
 var myVersion = GM_info.script.version;
