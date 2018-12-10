d10f705a09dbf57409f240cb4fcb27624917e2a8
svArtist <benjamin_philipp@gmx.de>
2018-12-10 10:45:23 +0000

    Adjusted for new search results DOM structure
    Used to be `#ires .g .rc h3.r a`; is now `#ires .g .rc .r a h3`

--- Full-Page-Titles-for-Google-Search.user.js
+++ Full-Page-Titles-for-Google-Search.user.js
@@ -1,7 +1,6 @@
 // ==UserScript==
 // @name		Retrieve Full Page Titles in Google Search
-// @version		1.10
-// @downloadURL	https://github.com/svArtist/Full-Page-Titles-for-Google-Search/raw/master/Full-Page-Titles-for-Google-Search.user.js
+// @version		1.6
 // @namespace	Google
 // @author		Benjamin Philipp <benjamin_philipp [at - please don't spam] gmx.de>
 // @description	Fill the page link titles with the full respective page titles
@@ -19,15 +18,14 @@ var settings = {};
 settings.applyToLinkText = false;			// Default = FALSE. TRUE = change innerHTML of links and applying overflow: visible to parent; false = only apply to Link Title (for mouseover Tooltip)
 settings.rex = "<title([^>]*)>([^<]+)<";	// Default = "<title([^>]*)>([^<]+)<". Regex to find the title of a page. If you find a better way, please let me know.
 settings.dontLookupExtensions = [".pdf"];	// Default = [".pdf"]. Exclude from lookup. PDFs are generally downloaded as files, giving you a popup. Excluding ".pdf" is recommended.
-settings.verbosity = 0;						// Default = 0. 0 = no logs; 1 = reports on link counts; 2 = +statuses of link checks; 3 = +Details
+settings.verbose = 3;						// Default = 1. 0 = no logs; 1 = reports on link counts; 2 = +statuses of link checks; 3 = +Details
 settings.keepSettings = true;				// Default = TRUE. TRUE = Try to save & load settings in browser's localStorage. FALSE = settings will be overweitten on update.
 settings.warnOnChange = true;				// Default = TRUE. TRUE = When changes are made but the Version number stays the same (assume changes by user), ask to save and apply the settings. FALSE = Automatically apply changes.
-settings.useTimerInsteadOfObserver = true;	// Default = TRUE. TRUE = Use a simple timer to check for links. Less likely to overlook updates, but will be running constantly.
-settings.checkTimer = 1000;					// Default = 1000. Interval for checking Links in milliseconds. Only relevant if useTimerInsteadOfObserver == TRUE.
 
 // Script vars, best don't touch
 var myVersion = GM_info.script.version;
 var settingsSafety = settings;
+var linkmatch = "#ires .g .rc .r a h3";
 var resultsObserver;
 var idle = true;
 var idletimer;
@@ -36,62 +34,57 @@ var updaterequest = false;
 var openRequests = 0;
 var successRequests = 0;
 var failedRequests = 0;
-var msgPrefix = "Full Page Titles in Google Search v" + myVersion + ":\n";
+var msgPrefix = "Full Page Titles in Google Search:\n";
+
+clog("Verbosity level: " + settings.verbose, 1);
 
 if(localStorage){
     var oldVersion = myVersion;
-    var storSettings = JSON.parse(localStorage.fptSettings);
-    clog(msgPrefix + "Old Version: " + localStorage.fptVersion,3);
-    clog(localStorage.fptSettings,3);
     if(localStorage.fptVersion !== undefined)
         var oldVersion = localStorage.fptVersion;
     else
         settingsSave();
 
-    if(oldVersion != myVersion && storSettings.keepSettings === true)
-        settingsLoad();
+    if(oldVersion != myVersion)
+        if(localStorage.fptSettings.keepSettings === true)
+            settingsLoad();
     if(settings.keepSettings === true){
-        if(oldVersion == myVersion && settingsChanged()){
-            if(settings.warnOnChange && !confirm(msgPrefix + "Settings have been changed, although the Script Version (" + oldVersion + ") has stayed the same.\nDo you want to save those settings to localStorage and use them?\nIf you didn't manually make new changes, localStorage has probably been lost. Simply click 'OK' to save the settings again.'"))
+        if(oldVersion == myVersion && settingsChanged() && settings.warnOnChange){
+            if (!confirm(msgPrefix + "Settings have been changed, although the Script Version (" + oldVersion + ") has stayed the same.\nDo you want to save those settings to localStorage and use them?\nIf you didn't manually make new changes, localStorage has probably been lost. Simply click 'OK' to save the settings again.'"))
                 settingsLoad();
         }
         settingsSave();
     }
+    else{
+        if(settings.keepSettings === true)
+            clog("Settings are set to work from localStorage, but localStorage is not available!\nMake sure this script has access to localStorage, or turn off this feature in the settings section of this script.",1);
+            alert(msgPrefix + "Settings are set to work from localStorage, but localStorage is not available!\nMake sure this script has access to localStorage, or turn off this feature in the settings section of this script.");
+    }
 
+    var tob = JSON.parse(localStorage.fptSettings);
     var complete = true;
     for(var key in settingsSafety){
         if(settingsSafety.hasOwnProperty(key)){
-           if(storSettings[key] === undefined || storSettings[key] === null){
+           if(tob[key] === undefined || tob[key] === null){
                clog("Added new settings object: " + key, 3);
-               storSettings[key] = settingsSafety[key];
+               tob[key] = settingsSafety[key];
                complete = false;
            }
         }
     }
     if(!complete){
-        localStorage.fptSettings = JSON.stringify(storSettings);
+        localStorage.fptSettings = JSON.stringify(tob);
     }
 }
-else
-{
-    if(settings.keepSettings === true){
-        clog("Settings are set to work from localStorage, but localStorage is not available!\nMake sure this script has access to localStorage, or turn off this feature in the settings section of this script.",1);
-        alert(msgPrefix + "Settings are set to work from localStorage, but localStorage is not available!\nMake sure this script has access to localStorage, or turn off this feature in the settings section of this script.");
-    }
-}
-
-clog(msgPrefix + "Verbosity level: " + settings.verbosity, 1);
 
 function settingsSave(){
     localStorage.fptSettings = JSON.stringify(settings);
     localStorage.fptVersion = oldVersion = myVersion;
-    clog("Settings saved",3);
 }
 function settingsLoad(){
     if(localStorage.fptSettings === undefined)
         return;
     settings = JSON.parse(localStorage.fptSettings);
-    clog("Settings loaded",3);
 }
 function settingsChanged(){
     if(localStorage.fptSettings === undefined || localStorage.fptSettings != JSON.stringify(settings))
@@ -100,27 +93,28 @@ function settingsChanged(){
 }
 
 
-openRequests = 0;
-successRequests = 0;
-failedRequests = 0;
-allinks = 0;
 function updatePage(){
+    openRequests = 0;
+    successRequests = 0;
+    failedRequests = 0;
+    var allinks = 0;
     clog("Walking through Links...", 2);
-	$("#ires .g .rc h3.r a:not(.done)").each(function(){
+	$(linkmatch + ":not([titled])").each(function(){
         allinks ++;
-        disableUpdate = true;
-		$(this).addClass("done");
-        disableUpdate = false;
-        clog("Looking at Link '" + this.href + "' (" + this.innerHTML + ")", 3);
-		if(this.text.substr(this.text.length-3)=="...")
-			getTitle(this);
+        clog("Looking at Link '" + $(this).parent()[0].href + "' (" + this.innerHTML + ")", 3);
+		console.log(this);
+		if(this.textContent.substr(this.textContent.length-3)=="..."){
+			clog("needs checking");
+			$(this).parent().css("background-color", "#fee");
+			getTitle($(this).parent()[0]);
+		}
 	});
-    clog(openRequests + " of " + allinks + " links are being checked.", 2);
+    clog(openRequests + " of " + allinks + " links need to be checked.", 1);
 }
 
 function getTitle(el){
     clog("Link '" + el.href + "' (" + el.innerHTML + ") is too long.", 3);
-	for(i=0; i<settings.dontLookupExtensions.length; i++)
+	for(var i=0; i<settings.dontLookupExtensions.length; i++)
 	{
 		if(el.href.endson(settings.dontLookupExtensions[i], true)){
             clog("Excluding Link '" + el.href + "' (" + el.innerHTML + ") because the extension (" + settings.dontLookupExtensions[i] + ") is excluded", 3);
@@ -131,21 +125,23 @@ function getTitle(el){
 	GM_xmlhttpRequest({
 		url: el.href,
 		method: "GET",
-		timeout: 30000, //30 seconds timeout
+		timeout: 15000, //15 seconds timeout
 		onload: function(res){
             var mrex = new RegExp(settings.rex, "i");
 			var tit = mrex.exec(res.response);
 			if(tit === undefined || tit === null){
-                clog({error: "No title found in response", page: el.href}, 2);
+                clog("No title found in response for " + el.href, 2);
                 report("fail");
 				return;
             }
 			disableUpdate = true;
 			el.title = unEscapeHtml(tit[2]);
+			$(el).css("background-color", "#efe");
             if(settings.applyToLinkText){
                 el.innerHTML = el.title;
                 $(el).parent().css("overflow", "visible");
             }
+			$(el).attr("titled", "true");
             report("success");
 			disableUpdate = false;
 		},
@@ -175,7 +171,7 @@ function report(status){
 }
 
 function clog(o, l=2){
-    if(settings.verbosity >= l)
+    if(settings.verbose >= l)
         console.log(o);
 }
 
@@ -207,6 +203,7 @@ function updater(t = 1000){
 	}
 	else
 	{
+        clog("Updater called but busy",3);
 		updaterequest = true;
 	}
 }
@@ -214,39 +211,38 @@ function updater(t = 1000){
 var bodyObserver;
 
 function observeResults(){
-	clog("observing contents", 3);
+	clog("observing", 3);
     updater();
 	resultsObserver = new MutationObserver(function(){
         clog("content changed",3);
         updater();
     });
-	resultsObserver.observe($("#ires")[0], {subtree: true, childList: true});
-	// if(bodyObserver !== undefined)
-	// 	bodyObserver.disconnect();
+	resultsObserver.observe($("#ires .g .rc")[0], {subtree: true, childList: true});
+	if(bodyObserver !== false)
+		bodyObserver.disconnect();
 }
 
 var oloc = window.location.href;
 function checkLocation(){
 	if(window.location.href != oloc){
-        clog("Window Location has changed (dynamic loading)",3);
+        clog("Window Location has changed (dynamic loading)",2);
 		oloc = window.location.href;
 		prepareObservers();
 	}
-	updater();
 }
 
 function prepareObservers(){
-    if($("#ires").length>0){
+    if($("#ires .g .rc").length>0){
         observeResults();
     }
     else{
-        bodyObserver = new MutationObserver(function(){
+        bodyObserver = new MutationObserver(function(mutations){
             if(disableUpdate || !idle){
                 return;
             }
-            if($("#ires").length>0)
+            if($("#ires .g .rc").length>0)
             {
-                clog("content found through body observer",3);
+                clog("content found through body observer");
                 observeResults();
             }
         });
@@ -254,10 +250,5 @@ function prepareObservers(){
     }
 }
 
-if(settings.useTimerInsteadOfObserver === true){
-	setInterval(updatePage, settings.checkTimer);
-}
-else{
-	setInterval(checkLocation, 1000);
-	prepareObservers();
-}
+setInterval(checkLocation, 1000);
+prepareObservers();
